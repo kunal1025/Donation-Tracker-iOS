d47531e03548a56325aa41e6c9b870f5646ed7a6
Kunal Patel <kunalp1025@gmail.com>
2018-12-02 19:37:58 -0500

    Initial Commit

---
+++ Donation Tracker iOS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Donation Tracker iOS
+//
+//  Created by Kunal Patel on 12/2/18.
+//  Copyright © 2018 SCRUM Bucket. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Donation Tracker iOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Donation Tracker iOS
+//
+//  Created by Kunal Patel on 12/2/18.
+//  Copyright © 2018 SCRUM Bucket. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
